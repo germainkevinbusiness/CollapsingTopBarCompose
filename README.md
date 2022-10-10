1c3aa226ce0b53ce254b995e6a1ffef461e6235f
germainkevinbusiness <67168053+kevingermainbusiness@users.noreply.github.com>
2022-10-09 23:13:59 -0400

    Release 1.1.0-beta03

--- README.md
+++ README.md
@@ -37,7 +37,7 @@ repositories {
 
 ```groovy
 dependencies {
-    implementation "com.github.germainkevinbusiness:CollapsingTopBarCompose:1.1.0-beta02"
+    implementation "com.github.germainkevinbusiness:CollapsingTopBarCompose:1.1.0-beta03"
 }
 ```
 
--- collapsingtopbar/src/main/java/com/germainkevin/collapsingtopbar/CollapsingTopBar.kt
+++ collapsingtopbar/src/main/java/com/germainkevin/collapsingtopbar/CollapsingTopBar.kt
@@ -19,34 +19,34 @@ import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import timber.log.Timber
 import kotlin.math.max
 import kotlin.math.roundToInt
 
 /**
  * [CollapsingTopBar] is like a [TopAppBar] that can collapse and/or expand.
  *
- * This [CollapsingTopBar] has slots for a title, subtitle, navigation icon, and actions.
+ * This [CollapsingTopBar] has slots to display a navigation icon, a title, a subtitle,
+ * a mainAction and actions.
  *
  * @param modifier A modifier that is passed down to the main layer which is a [Surface]
+ * @param navigationIcon The navigation icon displayed at the start of the [CollapsingTopBar].
  * @param title The title to be displayed inside the [CollapsingTopBar]
  * @param expandedTitle Optional, In case you want the [title] to look different when the
  * [CollapsingTopBar] is expanded
  * @param subtitle The subtitle to be displayed inside the [CollapsingTopBar]
- * @param navigationIcon the navigation icon displayed at the start of the [CollapsingTopBar].
  * This should typically be an [IconButton] or [IconToggleButton].
- * @param mainAction A composable that is added before [actions] that can be animated if
- * [scrollBehavior]'s [CollapsingTopBarScrollBehavior.centeredTitleAndSubtitle] is set to true
- * @param actions the actions displayed at the end of the [CollapsingTopBar]. This should typically
+ * @param mainAction A composable that is added before [actions] that animates as you collapse or
+ * expand the [CollapsingTopBar]
+ * @param actions The actions displayed at the end of the [CollapsingTopBar]. This should typically
  * be [IconButton]s. The default layout here is a [Row], so icons inside will be placed horizontally.
  * @param colors [CollapsingTopBarColors] that will be used to resolve the colors used for this
- * [CollapsingTopBar] in different states. See [CollapsingTopBarDefaults.colors].
+ * [CollapsingTopBar]. See [CollapsingTopBarDefaults.colors].
  * @param elevation The size of the shadow below the [Surface]
  * @param scrollBehavior determines the behavior of the [CollapsingTopBar]. If you want the
  * [CollapsingTopBar] to stay collapsed, you set it there, if you want the [CollapsingTopBar] to
  * have a different [collapsed height][CollapsingTopBarScrollBehavior.collapsedTopBarHeight] or
  * a different [expanded height][CollapsingTopBarScrollBehavior.expandedTopBarMaxHeight], you set it
- * there, if you want the [CollapsingTopBar] to detect when a scroll event has occured in your UI
+ * there, if you want the [CollapsingTopBar] to detect when a scroll event has occurred in your UI
  * and you want the [CollapsingTopBar] to collapse or expand, you simply pass
  * [scrollBehavior.nestedScrollConnection][CollapsingTopBarScrollBehavior.nestedScrollConnection] to
  * your Layout's [Modifier.nestedScroll][androidx.compose.ui.input.nestedscroll.nestedScroll].
@@ -89,19 +89,14 @@ fun CollapsingTopBar(
 }
 
 /**
- * @param modifier A modifier that is passed down to the main layer which is a [Surface]
- * @param title The title to be displayed inside the [CollapsingTopBar]
- * @param subtitle The subtitle to be displayed inside the [CollapsingTopBar], it's optional though
- * @param navigationIcon the navigation icon displayed at the start of the [CollapsingTopBar].
- * This should typically be an [IconButton] or [IconToggleButton].
- * @param actions the actions displayed at the end of the [CollapsingTopBar]. This should typically
- * be [IconButton]s. The default layout here is a [Row], so icons inside will be placed horizontally.
- * @param contentPadding The padding of the content inside the [CollapsingTopBar]
- * @param elevation The size of the shadow below the [Surface]
  * @param currentTopBarHeight The current height of the [CollapsingTopBar]
  * @param centeredTitleWhenCollapsed Whether the [title] should be centered when the
  * [CollapsingTopBar] is collapsed
- * @param elevation The size of the shadow below the [Surface]
+ * @param centeredTitleAndSubtitle Whether the [expandedTitle] and [subtitle] composables
+ * should be centered when the [CollapsingTopBar] is expanded
+ * @param expandedColumnAlphaValue The alpha visibility value of the [SimpleColumn] holding the
+ * [expandedTitle] and [subtitle] composables
+ * @param collapsedTitleAlpha The alpha visibility value of the [title] composable
  * */
 @Composable
 private fun CollapsingTopBarLayout(
@@ -125,37 +120,39 @@ private fun CollapsingTopBarLayout(
     colors: CollapsingTopBarColors,
     elevation: Dp,
 ) {
+    val scrollState = rememberScrollState()
     Surface(
         modifier = modifier
             .fillMaxWidth()
             .height(currentTopBarHeight)
-            .verticalScroll(rememberScrollState()),
+            .verticalScroll(scrollState),
         color = currentBackgroundColor.value,
         contentColor = currentContentColor,
         elevation = elevation,
     ) {
         Box {
-            val modifierWhenCentered = Modifier
-                .alpha(expandedColumnAlphaValue)
-                .fillMaxWidth()
-                .height(currentTopBarHeight)
-                .padding(horizontal = TopBarHorizontalPadding * 4)
-
-            val modifierWhenAtStart = Modifier
-                .alpha(expandedColumnAlphaValue)
-                .wrapContentWidth()
-                .height(currentTopBarHeight)
-                .align(Alignment.TopStart)
-                .padding(
-                    PaddingValues(
-                        start = if (navigationIcon != null) 56.dp - TopBarHorizontalPadding
-                        else TopBarTitleInset,
-                        end = TopBarHorizontalPadding,
+            val expandedColumnModifier = if (centeredTitleAndSubtitle) {
+                Modifier
+                    .alpha(expandedColumnAlphaValue)
+                    .fillMaxWidth()
+                    .height(currentTopBarHeight)
+                    .padding(horizontal = TopBarHorizontalPadding * 4)
+            } else {
+                Modifier
+                    .alpha(expandedColumnAlphaValue)
+                    .wrapContentWidth()
+                    .height(currentTopBarHeight)
+                    .align(Alignment.TopStart)
+                    .padding(
+                        PaddingValues(
+                            start = if (navigationIcon != null) 56.dp - TopBarHorizontalPadding
+                            else TopBarTitleInset,
+                            end = TopBarHorizontalPadding,
+                        )
                     )
-                )
+            }
             SimpleColumn(
-                modifier =
-                if (centeredTitleAndSubtitle) modifierWhenCentered else modifierWhenAtStart,
+                modifier = expandedColumnModifier,
                 horizontalAlignment =
                 if (centeredTitleAndSubtitle) Alignment.CenterHorizontally else Alignment.Start,
             ) {
@@ -190,10 +187,16 @@ private fun CollapsingTopBarLayout(
 
 /**
  * A simple version of [Column] created to hold the expanded title and subtitle
+ * @param modifier A modifier that is passed down to mainly to help determine the
+ * max width and height of the Layout
+ * @param content The content that is passed to the layout to be linearly and vertically positioned
+ * @param verticalArrangement How to arrange the [content]s vertically inside the [Layout]
+ * @param horizontalAlignment How to arrange to [content]s horizontally inside the [Layout]
+ * @author Germain Kevin
  * */
 @Composable
 private inline fun SimpleColumn(
-    modifier: Modifier = Modifier.wrapContentSize(),
+    modifier: Modifier = Modifier,
     verticalArrangement: Arrangement.Vertical = Arrangement.Top,
     horizontalAlignment: Alignment.Horizontal = Alignment.Start,
     content: @Composable () -> Unit,
@@ -234,7 +237,8 @@ private inline fun SimpleColumn(
 }
 
 /**
- * Holds the [navigationIcon], the [title], the [mainAction] and the [actions]
+ * A [Layout] that draws a [navigationIcon], a [title], a [mainAction] and [actions]
+ * @author Germain Kevin
  * */
 @Composable
 private fun SingleRowTopBar(
@@ -375,6 +379,14 @@ private fun TopBarLayout(
                 .measure(constraints.copy(maxWidth = maxTitleWidth))
 
         val layoutHeight = heightPx.roundToInt()
+
+//        val allHeights = listOf(
+//            navigationIconPlaceable.height,
+//            titlePlaceable.height, mainActionIconPlaceable.height,
+//            actionIconsPlaceable.height
+//        )
+//
+//        val longestPlaceable = allHeights.maxOrNull()
 
         layout(constraints.maxWidth, layoutHeight) {
 
